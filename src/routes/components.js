951ac5ec78016e14ac7aab6c514335282595e27f
fun <wentao.fang@zhenai.com>
2018-08-16 21:19:57 +0800

    echarts

--- src/routes/components.js
+++ src/routes/components.js
@@ -1,19 +1,5 @@
 export default {
     login: () => import("../views/login/login.vue"),
-    home: () => import("../views/home/home.vue")
+    home: () => import("../views/home/home.vue"),
+    echart: () => import("../views/echart/echart.vue")
 }
-
-// const login = () => import("../views/login/login.vue"),
-//       home = () => import("../views/home/home.vue");
-
-// import login from '../views/login/login.vue'
-// import home from '../views/home/home.vue'
-
-// export default {
-//     login,
-//     home
-// }
-// export default {
-//     login: resolve => require(['../views/login/login.vue'], resolve),
-//     home: resolve => require(["../views/home/home.vue"], resolve)
-// }
